0e5445b289f6667397c87950dbc3bb326fc2b404
tort32 <tort.biz@gmail.com>
2025-01-09 16:31:23 +0100

    Add board config for SlimeVR mini P2
    Configure sensor axis directions for SlimeVR mini boards

--- src/globals.h
+++ src/globals.h
@@ -10,9 +10,24 @@
 #define IGNORE_RESET CONFIG_IGNORE_RESET // If sw0 available, don't change any reset behavior
 #define WOM_USE_DCDC CONFIG_WOM_USE_DCDC // Use DCDC instead of LDO for WOM if it is more efficient
 
+#if defined(CONFIG_BOARD_SLIMEVRMINI_P1_UF2) || defined(CONFIG_BOARD_SLIMEVRMINI_P2_UF2)
+#define SENSOR_GYROSCOPE_AXES_ALIGNMENT gy, -gz, -gx
+#define SENSOR_ACCELEROMETER_AXES_ALIGNMENT ay, -az, -ax
+#define SENSOR_MAGNETOMETER_AXES_ALIGNMENT -mx, mz, -my
+#define SENSOR_QUATERNION_CORRECTION 0.7071f, 0.7071f, 0.0f, 0.0f
+#endif
+
+#ifndef SENSOR_GYROSCOPE_AXES_ALIGNMENT
 #define SENSOR_GYROSCOPE_AXES_ALIGNMENT gx, -gz, gy // gyro axes alignment to sensor body
+#endif
+#ifndef SENSOR_ACCELEROMETER_AXES_ALIGNMENT
 #define SENSOR_ACCELEROMETER_AXES_ALIGNMENT ax, -az, ay // accel axes alignment to sensor body
+#endif
+#ifndef SENSOR_MAGNETOMETER_AXES_ALIGNMENT
 #define SENSOR_MAGNETOMETER_AXES_ALIGNMENT my, mz, -mx // mag axes alignment to sensor body
+#endif
+#ifndef SENSOR_QUATERNION_CORRECTION
 #define SENSOR_QUATERNION_CORRECTION 0.5f, -0.5f, -0.5f, -0.5f // correction quat for sensor to mounting orientation
+#endif
 
 #endif
